e182ce6849d794deb92795f8b6600db38a1d7a3a
mauro saavedra <saa.mauro92@gmail.com>
2023-05-20 19:24:14 +0100

    add name validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,9 @@ document.addEventListener("DOMContentLoaded", function () {
   
   playButton.addEventListener("click", function(){
       player = document.getElementById("player-name").value;
-
+      if(!player){
+        return document.getElementById("display-name-error").innerHTML = "**Please complete your name"
+    }
       document.getElementById("title").classList.add('moved');
       document.getElementById("game-options").style.display = 'none';
       document.getElementById("game").style.display = "flex"; 
