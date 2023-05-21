1bf7097c1e5b5dd66a441607993c8c7649eecf57
mauro saavedra <saa.mauro92@gmail.com>
2023-05-21 13:50:13 +0100

    add wronletter array and modify function to get the wrong letters

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,35 @@
 var category = "countries";
 var choosenWord;
+let wrongLetters = [];
 var gameStatus = "off";
-
+var alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 document.addEventListener("DOMContentLoaded", function () {
   let playButton = document.getElementById("play-button");
   /** This event sets the game taking player name and category in order to start*/
@@ -24,139 +52,126 @@ document.addEventListener("DOMContentLoaded", function () {
     choosenWord = getRandomWordBycategory(category);
     gameStatus = "on";
   });
-
-
 });
 
 // Gets the key value when pressed
 document.addEventListener("keydown", function (event) {
-    if(gameStatus === "on"){
-        let key = event.key;
-        checkIfWordMatchesKey(choosenWord, key);
-        console.log(key);
-    }
-  });
-
-
+  if (gameStatus === "on") {
+    let key = event.key;
+    checkIfWordMatchesKey(choosenWord, key);
+    console.log(key);
+  }
+});
 
 /** This function gets a new or current category and returns a random word */
 function getRandomWordBycategory(category) {
-    let choosenWord;
-     if (category === "animals") {
-       choosenWord =
-         animals[Math.floor(Math.random() * animals.length)].word;
-         runGame(choosenWord)
-     } else if (category === "movies") {
-       choosenWord =
-         movies[Math.floor(Math.random() * movies.length)].word;
-         runGame(choosenWord)
-     } else {
-       choosenWord =
-         countries[
-           Math.floor(Math.random() * countries.length)
-         ].word;
-         runGame(choosenWord)
-     }
-   console.log(choosenWord);
-     return choosenWord;
-   }
-
-
-/** This function gets a random word and display letter spaces in the ui */
-   function runGame(choosenWord) {
-    let lettersContainer = document.getElementById("word-container");
-    lettersContainer.innerHTML = "";
-  
-    let html = `<div class="game-letter"></div>`;
-    let htmlEmpty = `<div class="game-letter-space"></div>`;
-  
-    for (let i = 0; i < choosenWord.length; i++) {
-      if (choosenWord[i] === " ") {
-        lettersContainer.innerHTML += htmlEmpty;
-        continue;
-      } else {
-        lettersContainer.innerHTML += html;
-      }
-    }
+  let choosenWord;
+  if (category === "animals") {
+    choosenWord = animals[Math.floor(Math.random() * animals.length)].word;
+    runGame(choosenWord);
+  } else if (category === "movies") {
+    choosenWord = movies[Math.floor(Math.random() * movies.length)].word;
+    runGame(choosenWord);
+  } else {
+    choosenWord = countries[Math.floor(Math.random() * countries.length)].word;
+    runGame(choosenWord);
+  }
+  console.log(choosenWord);
+  return choosenWord;
 }
 
+/** This function gets a random word and display letter spaces in the ui */
+function runGame(choosenWord) {
+  let lettersContainer = document.getElementById("word-container");
+  lettersContainer.innerHTML = "";
 
+  let html = `<div class="game-letter"></div>`;
+  let htmlEmpty = `<div class="game-letter-space"></div>`;
 
-
-
+  for (let i = 0; i < choosenWord.length; i++) {
+    if (choosenWord[i] === " ") {
+      lettersContainer.innerHTML += htmlEmpty;
+      continue;
+    } else {
+      lettersContainer.innerHTML += html;
+    }
+  }
+}
 
 function checkIfWordMatchesKey(word, key) {
-    if (gameStatus === "off") return;
-    let indexes = [];
-    let wordArray = [];
+  if (gameStatus === "off") return;
+  let indexes = [];
+  let wordArray = [];
 
-    //Makes an array of the choosenWord in order to match the indexes in case there is a space on the word using the wordarray[]
-    if (word !== null) {
-        for (let letter of word) {
-          if (letter === " ") {
-            continue;
-          } else {
-            wordArray.push(letter);
-          }
-        }
-  
-      //Get indices of letters matched using the indexexs[]
-      for (let i = 0; i < wordArray.length; i++) {
-        if (wordArray[i] === " ") {
-          continue;
-        } else if (wordArray[i] === key) {
-          indexes.push(i);
-
-        }
+  //Makes an array of the choosenWord in order to match the indexes in case there is a space on the word using the wordarray[]
+  if (word !== null) {
+    for (let letter of word) {
+      if (letter === " ") {
+        continue;
+      } else {
+        wordArray.push(letter);
       }
-  
+    }
+
+    //Get indices of letters matched using the indexexs[]
+    for (let i = 0; i < wordArray.length; i++) {
+      if (wordArray[i] === " ") {
+        continue;
+      } else if (wordArray[i] === key) {
+        indexes.push(i);
+      }
+    }
+  }
+
+  //Check for wrong letters
+  if (
+    !wordArray.includes(key) &&
+    !wrongLetters.includes(key) &&
+    alphabet.includes(key)
+  ) {
+    wrongLetters.push(key);
+    console.log(wrongLetters);
   }
 
   console.log(wordArray);
   printLetter(indexes, wordArray);
 }
 
-
 /** This function takes indexes of correct letters and the word as an array to display them in the interface*/
 function printLetter(indexes, word) {
-    let letters = document.getElementsByClassName("game-letter");
-    for (let index of indexes) {
-      letters[index].textContent = word[index];
-    }
+  let letters = document.getElementsByClassName("game-letter");
+  for (let index of indexes) {
+    letters[index].textContent = word[index];
   }
-
-
-
-
-
+}
 
 let countries = [
-    {
-      word: "argentina",
-      hint: "he largest country in South America, famous for its vibrant culture, soccer, and the Amazon Rainforest",
-    },
-    { word: "united states", hint: "sjhgjhgth american" },
-    { word: "germany", hint: "south amerjhgjhgican" },
-    { word: "italy", hint: "south amerjhgjhican" },
-    { word: "siri lanka", hint: "south amejhgjhgjhgrican" },
-    { word: "spain", hint: "south amerjhgjhgican" },
-  ];
-  let animals = [
-    { word: "cat", hint: "soufhgfhgfth amejhgjhgjhgjhgjrican" },
-    { word: "dog", hint: "souhgfhgth american" },
-    { word: "rino", hint: "souhgfhgfhth american" },
-    { word: "cocodrile", hint: "soguth american" },
-    { word: "elephant", hint: "southgfh american" },
-    { word: "komodo dragon", hint: "sohgfhgfuth american" },
-  ];
-  
-  let movies = [
-    {
-      word: "The Godfather",
-      hint: "A classic crime film directed by Francis Ford Coppola, known for its iconic characters and memorable quotes like 'I'm gonna make him an offer he can't refuse'.",
-    },
-    {
-      word: "Star Wars",
-      hint: "An epic space opera franchise created by George Lucas, featuring iconic characters like Luke Skywalker, Darth Vader, and Yoda.",
-    },
-  ];
+  {
+    word: "argentina",
+    hint: "he largest country in South America, famous for its vibrant culture, soccer, and the Amazon Rainforest",
+  },
+  { word: "united states", hint: "sjhgjhgth american" },
+  { word: "germany", hint: "south amerjhgjhgican" },
+  { word: "italy", hint: "south amerjhgjhican" },
+  { word: "siri lanka", hint: "south amejhgjhgjhgrican" },
+  { word: "spain", hint: "south amerjhgjhgican" },
+];
+let animals = [
+  { word: "cat", hint: "soufhgfhgfth amejhgjhgjhgjhgjrican" },
+  { word: "dog", hint: "souhgfhgth american" },
+  { word: "rino", hint: "souhgfhgfhth american" },
+  { word: "cocodrile", hint: "soguth american" },
+  { word: "elephant", hint: "southgfh american" },
+  { word: "komodo dragon", hint: "sohgfhgfuth american" },
+];
+
+let movies = [
+  {
+    word: "The Godfather",
+    hint: "A classic crime film directed by Francis Ford Coppola, known for its iconic characters and memorable quotes like 'I'm gonna make him an offer he can't refuse'.",
+  },
+  {
+    word: "Star Wars",
+    hint: "An epic space opera franchise created by George Lucas, featuring iconic characters like Luke Skywalker, Darth Vader, and Yoda.",
+  },
+];
