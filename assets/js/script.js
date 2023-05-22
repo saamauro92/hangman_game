f413ce25444fbbe9b4596492e4ae6ea619036857
mauro saavedra <saa.mauro92@gmail.com>
2023-05-22 12:32:14 +0100

    remove fake data of countries animals and movies

--- assets/js/script.js
+++ assets/js/script.js
@@ -357,33 +357,6 @@ function animationByClass(className) {
   }
 }
 
-let countries = [
-  {
-    word: "argentina",
-    hint: "he largest country in South America, famous for its vibrant culture, soccer, and the Amazon Rainforest",
-  },
-  { word: "united states", hint: "sjhgjhgth american" },
-  { word: "germany", hint: "south amerjhgjhgican" },
-  { word: "italy", hint: "south amerjhgjhican" },
-  { word: "siri lanka", hint: "south amejhgjhgjhgrican" },
-  { word: "spain", hint: "south amerjhgjhgican" },
-];
-let animals = [
-  { word: "cat", hint: "soufhgfhgfth amejhgjhgjhgjhgjrican" },
-  { word: "dog", hint: "souhgfhgth american" },
-  { word: "rino", hint: "souhgfhgfhth american" },
-  { word: "cocodrile", hint: "soguth american" },
-  { word: "elephant", hint: "southgfh american" },
-  { word: "komodo dragon", hint: "sohgfhgfuth american" },
-];
-
-let movies = [
-  {
-    word: "The Godfather",
-    hint: "A classic crime film directed by Francis Ford Coppola, known for its iconic characters and memorable quotes like 'I'm gonna make him an offer he can't refuse'.",
-  },
-  {
-    word: "Star Wars",
-    hint: "An epic space opera franchise created by George Lucas, featuring iconic characters like Luke Skywalker, Darth Vader, and Yoda.",
-  },
-];
+let countries;
+let animals;
+let movies;
